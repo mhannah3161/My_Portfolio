d88740e66588b0cf26e456b081afca7657353d5a
Michael Hannah <Michaelrkhannah@gmail.com>
2023-11-27 14:49:17 -0700

    fixed referancing err in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React, {useEffect, useState } from 'react'
 import './styles/tailwind.css'
-import About from './components/About.jsx'
 import Portfolio from './components/Portfolio.jsx'
 import Contact from './components/Contact.jsx'
 import Footer from './components/Footer.jsx'
