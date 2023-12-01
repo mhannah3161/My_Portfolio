b9c7d6c60df5f374de495faf2a1efd4bacd18d3e
Michael Hannah <Michaelrkhannah@gmail.com>
2023-11-30 21:04:23 -0700

    added uppercasing...

--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import timeline from './data/timeline';
+import timeline from './Data/timeline';
 import TimelineItem from './TimelineItem';
 import Title from './Title';
 
